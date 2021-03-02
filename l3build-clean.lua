6859234293fcbbb276bd1523cadc7ad86274f8fd
LAURENS Jérôme <jerome.laurens@u-bourgogne.fr>
2021-03-03 00:28:19 +0100

    clean module refactor

--- l3build-clean.lua
+++ l3build-clean.lua
@@ -37,52 +37,52 @@ local remove_tree           = fslib.remove_tree
 local make_clean_directory  = fslib.make_clean_directory
 local remove_directory      = fslib.remove_directory
 
+---@type l3b_aux_t
+local l3b_aux = require("l3b.aux")
+local call    = l3b_aux.call
+
 -- Remove all generated files
 local function clean()
   -- To make sure that distribdir never contains any stray subdirs,
   -- it is entirely removed then recreated rather than simply deleting
   -- all of the files
-  local errorlevel =
-    remove_directory(distribdir)    +
-    make_directory(distribdir)    +
-    make_clean_directory(localdir)   +
-    make_clean_directory(testdir)    +
-    make_clean_directory(typesetdir) +
-    make_clean_directory(unpackdir)
+  local error_level = remove_directory(distribdir)
+                    + make_directory(distribdir)
+                    + make_clean_directory(localdir)
+                    + make_clean_directory(testdir)
+                    + make_clean_directory(typesetdir)
+                    + make_clean_directory(unpackdir)
 
-  if errorlevel ~= 0 then return errorlevel end
+  if error_level ~= 0 then return error_level end
 
   local clean_list = {}
   for dir in unique_items(maindir, sourcefiledir, docfiledir) do
     for glob in entries(cleanfiles) do
-      for file in keys(tree(dir,glob)) do
+      for file in keys(tree(dir, glob)) do
         clean_list[file] = true
       end
     end
     for glob in entries(sourcefiles) do
-      for file in keys(tree(dir,glob)) do
+      for file in keys(tree(dir, glob)) do
         clean_list[file] = nil
       end
     end
     for file in keys(clean_list) do
-      errorlevel = remove_tree(dir,file)
-      if errorlevel ~= 0 then return errorlevel end
+      error_level = remove_tree(dir, file)
+      if error_level ~= 0 then return error_level end
     end
   end
-
   return 0
 end
 
 local function bundle_clean()
-  local errorlevel = call(modules, "clean")
-  for i in entries(cleanfiles) do
-    errorlevel = remove_tree(currentdir, i) + errorlevel
+  local error_level = call(modules, "clean")
+  for g in entries(cleanfiles) do
+    error_level = error_level + remove_tree(currentdir, g)
   end
-  return (
-    errorlevel     +
-    remove_directory(ctandir) +
-    remove_directory(tdsdir)
-  )
+  return  error_level
+        + remove_directory(ctandir)
+        + remove_directory(tdsdir)
 end
 
 -- this is the map to export function symbols to the global space
@@ -96,8 +96,8 @@ extend_with(_G, global_symbol_map)
 -- [=[ ]=]
 
 ---@class l3b_clean_t
----@field clean function
----@field bundle_clean function
+---@field clean         fun(): integer
+---@field bundle_clean  fun(): integer
 
 return {
   global_symbol_map = global_symbol_map,
